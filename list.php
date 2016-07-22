1895988dd6976e6b84ff2fe58c01c3b5876e8b89
karthikarul20 <karthikarul20@gmail.com>
2016-07-22 20:43:57 +0800

    Pointing to the php-master - now able to write docs file

--- list.php
+++ list.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/includes/JSLikeHTMLElement.php';
-require_once __DIR__ . '/includes/PHPWord-0.12.1/src/PhpWord/Autoloader.php'; 
+require_once __DIR__ . '/includes/PHPWord-master/src/PhpWord/Autoloader.php'; 
 /**
  * Header file
  */
@@ -11,10 +11,11 @@ use PhpOffice\PhpWord\Element\Image as Image;
 use PhpOffice\PhpWord\Media;
 
 Autoloader::register();
-Settings::loadConfig();
 
 date_default_timezone_set ( 'UTC' );
-ini_set ( 'display_errors', 1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ini_set ( 'max_execution_time', 0 );
 $name='list';
 $file = __DIR__ . "/html/{$name}.html";
@@ -34,19 +35,24 @@ $dom->loadHTML( $htmlContent );
 // libxml_use_internal_errors($internalErrors);
 
 
+// Creating the new document...
 $phpWord = new \PhpOffice\PhpWord\PhpWord();
 
+/* Note: any element you append to a document must reside inside of a Section. */
+
+// Adding an empty Section to the document...
 $section = $phpWord->addSection();
 // Adding Text element to the Section having font styled by default...
-$section->addText('Sample List Conversion');
+$section->addText(
+    htmlspecialchars('Test Sample List')
+);
 
 
-
-function processLi($child){
+function processLi($child, $section){
 		
     if (strtolower ( $child->nodeName ) == 'ol' || strtolower ( $child->nodeName ) == 'ul')
     {
-        // $section->addText('list - '.$child->nodeName);
+        $section->addText('list - '.$child->nodeName);
         print_r('*');
         echo "\r\n";
         if($child->hasChildNodes()) {
@@ -64,12 +70,12 @@ function processLi($child){
                     foreach ($li->childNodes  as $liChild  ) {
                         echo "\t";
                         print_r($li->firstChild->nodeName);
-                        processLi($liChild);
+                        processLi($liChild, $section);
                     }
                 }
                 else if (strtolower ( $li->nodeName ) == 'li')
                 {
-                    //$section->addListItem($li->nodeName);
+                    $section->addListItem($li->nodeName.' - '.$li->textContent);
                     echo ' --- ';
                     print_r($li->textContent); 
                 }                            
@@ -85,19 +91,18 @@ $xpath->registerNamespace( 'php', 'http://php.net/xpath' );
 $xpath->registerPhpFunctions( array( 'preg_match', 'preg_split','preg_replace', 'sizeof', 'str_word_count' ) );
 
 $results = $xpath->query ( '//div[contains(@class,"body")]' );
-
+echo 'before creating......';
 
 foreach ( $results as $resultNode ) {
     if (strtolower ( $resultNode->nodeName ) == 'div') {
         
         if($resultNode->hasChildNodes()) {
             $children = $resultNode->childNodes;
-            print_r($children);
             foreach ( $children as $child ) {
                 // create recursive for html element childs
                 echo "\r\n";
                 print_r($child->nodeName);
-                processLi($child);
+                processLi($child, $section);
                
             }
         } 
@@ -106,17 +111,19 @@ foreach ( $results as $resultNode ) {
 }
 
 
-$exportFile= __DIR__ . "/word/{$name}.docx";
+$exportFile=  __DIR__ ."/word/{$name}.docx";
+
+// print_r($section);
 
 
-echo 'before creating......';
+
+
 // Saving the document as OOXML file...
 // $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+$phpWord->save($exportFile, 'Word2007');
 
-// $objWriter->save($exportFile);
-// $phpWord->save( $exportFile, 'Word2007' ); //save as Document
 
-$count=0;
+// $count=1/0;
 echo "\r";
 echo "\r";
 echo '**************************';
